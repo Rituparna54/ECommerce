964d7295e35cb5fce28af3437b7f6899908597dd
Rituparna Saha <rituparnasaha54@gmail.com>
2023-04-06 23:17:18 +0530

    Project Complete

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,17 +1,30 @@
-import { Component  , OnInit} from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { User } from '../models/models';
+import { NavigationService } from '../services/navigation.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
-  invalidRPWD: boolean=false;
-  constructor(private fb: FormBuilder){}
+  invaildRPWD: boolean = false;
+  message = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private navigationService: NavigationService
+  ) {}
+
   ngOnInit(): void {
-    this.registerForm= this.fb.group({
+    this.registerForm = this.fb.group({
       firstName: [
         '',
         [
@@ -28,8 +41,10 @@ export class RegisterComponent implements OnInit{
           Validators.pattern('[a-zA-Z].*'),
         ],
       ],
-      email:['',[Validators.required , Validators.email]],
-      pwd:[
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', [Validators.required]],
+      mobile: ['', Validators.required],
+      pwd: [
         '',
         [
           Validators.required,
@@ -37,11 +52,29 @@ export class RegisterComponent implements OnInit{
           Validators.maxLength(15),
         ],
       ],
-      rpwd:[''],
+      rpwd: [''],
     });
   }
-  register(){}
 
+  register() {
+    let user: User = {
+      id: 0,
+      firstName: this.FirstName.value,
+      lastName: this.LastName.value,
+      email: this.Email.value,
+      address: this.Address.value,
+      mobile: this.Mobile.value,
+      password: this.PWD.value,
+      createdAt: '',
+      modifiedAt: '',
+    };
+
+    this.navigationService.registerUser(user).subscribe((res: any) => {
+      this.message = res.toString();
+    });
+  }
+
+  //#region Getters
   get FirstName(): FormControl {
     return this.registerForm.get('firstName') as FormControl;
   }
@@ -51,11 +84,17 @@ export class RegisterComponent implements OnInit{
   get Email(): FormControl {
     return this.registerForm.get('email') as FormControl;
   }
+  get Address(): FormControl {
+    return this.registerForm.get('address') as FormControl;
+  }
+  get Mobile(): FormControl {
+    return this.registerForm.get('mobile') as FormControl;
+  }
   get PWD(): FormControl {
     return this.registerForm.get('pwd') as FormControl;
   }
   get RPWD(): FormControl {
     return this.registerForm.get('rpwd') as FormControl;
   }
-
+  //#endregion
 }
